155959dae6d2fbcea034ec38a00f3d199c487e6a
liudian <hardy.liu@outlook.com>
2017-11-01 11:23:36 +0800

    update: 完成platform平台/api/api/client_feedback接口的迁移

--- app/Http/Controllers/Platform/ClientController.php
+++ app/Http/Controllers/Platform/ClientController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Platform;
 
+use App\Models\Platform\ClientFeedback;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class ClientController extends Controller
 {
@@ -14,7 +16,46 @@ class ClientController extends Controller
 
     public function collectClientFeedback(Request $request)
     {
-        $path = $request->file('test')->store('feedback', 'platform');
-        return $path;
+        $this->filterFeedbackForm($request);
+
+        if ($request->hasFile('img')) {
+            $img = $request->file('img')->store('feedback', 'platform');
+        } else {
+            $img = '';
+        }
+
+        $feedback = ClientFeedback::where('rid', $request->rid)
+            ->where('type', $request->type)
+            ->first();
+
+        if (empty($feedback)) {
+            ClientFeedback::create([
+                'rid' => $request->rid,
+                'type' => $request->type,
+                'content' => $request->input('content') ?: '',
+                'img' => $img,
+                'create_time' => time(),
+            ]);
+        } else {
+            $feedback->update([
+                'content' => $request->input('content') ?: '',
+                'img' => $img,
+                'create_time' => time(),
+            ]);
+        }
+
+        return [
+            'code' => 0,
+        ];
+    }
+
+    protected function filterFeedbackForm($request)
+    {
+        $this->validate($request, [
+            'rid' => 'required|integer',
+            'type' => 'required|integer',
+            'content' => 'string|max:255',
+            'img' => 'mimes:jpeg,jpg,bmp,png,gif|max:2048',
+        ]);
     }
 }
