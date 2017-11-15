71b4d924b1afe55f522cbc48708f781e6e4e5733
liudian <hardy.liu@outlook.com>
2017-11-15 14:44:38 +0800

    update: 优化管理员后台的side.js代码

--- client/src/pages/admin/sidebar.js
+++ client/src/pages/admin/sidebar.js
@@ -101,17 +101,22 @@ new Vue({
     },
   },
 
-  methods: {},
+  methods: {
+    activateMenu () {   //active 当前访问的uri在sidebar中的菜单项
+      let _self = this
+      let currentUrl = _.trim(location.href, '/')   //去掉尾部的'/'
+      let currentUri = currentUrl.match(/http:\/\/[\w.-]+\/admin\/([\w/-]+)/)[1]
+        .split('/')
+
+      //被访问的页面的菜单项会被设置为active
+      currentUri.reduce(function (lastValue, currentValue) {
+        lastValue[currentValue].isActive = true
+        return lastValue[currentValue]
+      }, _self.uri)
+    },
+  },
 
   created: function () {
-    let _self = this
-    let currentUri = location.href.match(/http:\/\/[\w.-]+\/admin\/([\w/-]+)/)[1]
-      .split('/')
-
-    //被访问的页面的菜单项会被设置为active
-    currentUri.reduce(function (lastValue, currentValue) {
-      lastValue[currentValue].isActive = true
-      return lastValue[currentValue]
-    }, _self.uri)
+    this.activateMenu()
   },
 })
